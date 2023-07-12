97eee5564bc48ee06f50aeeaa51b9afdd4dbee75
Carlos Larios <20460440@colima.tecnm.mx>
2023-07-12 00:12:48 -0600

    First Commit

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla a quibusdam ullam similique debitis libero dolore minima molestiae assumenda sequi? Nobis corporis, asperiores atque expedita aut excepturi deserunt, iure dolor magnam hic fugiat quis totam nihil culpa? Vel ratione fuga dolorem consequuntur, laborum vero ea deserunt ducimus mollitia voluptate velit esse cupiditate accusantium perferendis cum voluptatibus sapiente voluptas ex saepe consequatur distinctio vitae perspiciatis ab! In nobis tempore neque voluptatem qui veritatis veniam quo voluptas optio esse ex, at dolor distinctio consectetur expedita, ab quas fugit quisquam. Reiciendis maxime nesciunt ea nisi, repellendus, velit nihil est labore, ipsum neque cupiditate.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Sevices</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto architecto sint vero, quidem minima porro tenetur qui eius enim exercitationem pariatur libero sit excepturi non accusamus dolore? Obcaecati debitis, doloribus earum quod eos maxime natus ipsam sequi, nihil facilis pariatur. Explicabo, veritatis. Eos mollitia quod esse, nesciunt quo fugit ab?</p>
+    </div>
+  )
+}
+
+export default ServicesPage
